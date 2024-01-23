4c697ea8b31ceb71edaccc6cd73da22e9b914866
resojmc <resojmc@gmail.com>
2024-01-23 13:34:56 -0800

    Main

---
+++ index.js
@@ -0,0 +1,41 @@
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-id");
+const menuBtnIcon = menuBtn.querySelector("i");
+
+menuBtn.addEventListener("click" , (e) => 
+{
+  navLinks.classList.toggle("open");
+
+  const isOpen = navLinks.classList.contains("open");
+  menuBtnIcon.setAttribute(
+    "class",
+    isOpen ? "ri-close-line" : "ri-menu-3-line"
+  );
+});
+
+navLinks.addEventListener("click", (e) =>
+{
+  navLinks.classList.remove("open");
+  menuBtnIcon.setAttribute("class", "ri-menu-3-line");
+});
+
+
+
+
+function sendEmail() {
+    const params = {
+      to_email: document.getElementById("email-input").value,
+    };
+    const serviceID = "service_c6688w6";
+    const templateID = "template_3hg8c6g";
+
+    emailjs
+      .send(serviceID, templateID, params)
+      .then((clearInput) => {
+        document.getElementById("email-input").value = "";
+        alert("Message Sent Successfully!");
+      })
+      .catch((err) => {
+        alert("Error. Message was not sent.");
+      });
+  }
